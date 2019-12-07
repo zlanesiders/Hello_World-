c62afe9743cc0bb439ad09f930d5bec6eb0beff9
zlanesiders <58625763+zlanesiders@users.noreply.github.com>
2019-12-07 07:21:14 -0500

    First Code
    First super basic code I have done in years.

---
+++ practice.cxx
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hey there planet!" <<endl;
+	return 0;
+}
---
+++ practice.py
@@ -0,0 +1,8 @@
+import math
+
+print("Hello World!")
+
+x = 0
+while x < 5:
+	x += 1
+	print(x)
